7daf5ff4218efece39b287c8e274cd1d326efd45
wujunzhong <junzhong.wu@zun1.com>
2018-06-05 19:51:31 +0800

    'debug'

--- src/RapToPostman.php
+++ src/RapToPostman.php
@@ -18,7 +18,7 @@ class RapToPostman
     public function run($rapBackupSrc, $savePath)
     {
         /*rap文档数据获取*/
-        $data = file_get_contents($rapSrc);
+        $data = file_get_contents($rapBackupSrc);
         $data = json_decode($data, true);
         $data['modelJSON'] = str_replace("\'", '\"', $data['modelJSON']);
         $data = json_decode($data['modelJSON'], true);
@@ -38,21 +38,38 @@ class RapToPostman
                 foreach ($page['actionList'] as &$action) {
                     $a = [];
                     $a['name'] = $action['name']; // 接口名称
-                    $a['event'] = $this->event(); // 接口测试反馈event
+                    $a['event'] = $this->event(); // 接口测试反馈 event
                     // 请求信息
-                    $a['request']['url'] = $this->requestUrl($action['requestUrl']); // 接口地址
                     $a['request']['method'] = $this->requestType($action['requestType']); // 请求方式
-                    $a['request']['header'] = $this->requestHeader(); // 请求头部header
-                    $a['request']['description'] = $action['description'];// 接口描述
-                    // 请求体body
-                    $a['request']['body']['mode'] = 'urlencoded';
+                    $a['request']['header'] = $this->requestHeader(); // 请求头部 header
+                    $a['request']['description'] = $action['description']; // 接口描述
+                    $url = "{{api_url}}" . $this->requestUrl($action['requestUrl']); // 接口地址
+                    // 请求体 or 查询参数
                     $list = [];
                     foreach ($action['requestParameterList'] as $param) {
                         $r = [];
                         $this->param($param, $param['identifier'], $r);
                         $list = array_merge($list, $r);
                     }
-                    $a['request']['body']['urlencoded'] = $list;
+                    switch ($a['request']['method']) {
+                        case 'GET':
+                            $a['request']['url']['query'] = $list;
+                            $a['request']['url']['raw'] = $a['request']['url']['host'] = $url;
+                            break;
+                        case 'PUT':
+                            $a['request']['body']['mode'] = 'urlencoded';
+                            $a['request']['body']['urlencoded'] = $list;
+                            $a['request']['url'] = $url;
+                            break;
+                        case 'POST':
+                            $a['request']['body']['mode'] = 'formdata';
+                            $a['request']['body']['formdata'] = $list;
+                            $a['request']['url'] = $url;
+                            break;
+                        case 'DELETE':
+                            $a['request']['url'] = $url;
+                            break;
+                    }
                     $p['item'][] = $a;
                 }
                 $postMan['item'][] = $p;
@@ -63,7 +80,9 @@ class RapToPostman
             mkdir($savePath);
             chmod($savePath, 0777);
         }
-        file_put_contents($savePath."/postman.json", json_encode($postMan));
+        file_put_contents($savePath . "/postman.json", json_encode($postMan));
+
+        return $savePath . "/postman.json";
     }
 
     /**
@@ -82,45 +101,50 @@ class RapToPostman
                 $url = str_replace($v, $re, $url);
             }
         }
+
         return $url;
     }
 
     /**
      * 请求参数转换
      */
-    private function param($param, $code='', &$list)
+    private function param($param, $code = '', &$list)
     {
         if (!$param) {
             return true;
         }
         $r = [
-            'type'    => 'text',
-            'enabled' => true
+            'type' => 'text',
+            'enabled' => true,
+            'description' => '',
+            'value' => '',
         ];
         $type = $param['dataType'];
         switch ($type) {
             case 'object':
                 foreach ($param['parameterList'] as $v) {
-                    $this->param($v, $code.'['. $v['identifier'] .']', $list);
+                    $this->param($v, $code . '[' . $v['identifier'] . ']', $list);
                 }
                 break;
             case 'array<object>':
                 foreach ($param['parameterList'] as $v) {
-                    $this->param($v, $code.'[0]['. $v['identifier'] .']', $list);
+                    $this->param($v, $code . '[0][' . $v['identifier'] . ']', $list);
                 }
                 break;
             case 'number':
             case 'string':
             case 'boolean':
                 $r['key'] = $code;
-                list($remark, $mock) = explode('@mock=', $param['remark']);
-                $r['value'] = $mock ? $mock : '';
+                $remark = explode('@mock=', $param['remark']);
+                $r['description'] = $param['name'] . '。' . $remark[0];
+                $r['value'] = isset($remark[1]) ? $remark[1] : '';
                 $list[] = $r;
                 break;
             default:
                 $r['key'] = $code . '[0]';
-                list($remark, $mock) = explode('@mock=', $param['remark']);
-                $r['value'] = $mock ? $mock : '';
+                $remark = explode('@mock=', $param['remark']);
+                $r['description'] = $param['name'] . '。' . $remark[0];
+                $r['value'] = isset($remark[1]) ? $remark[1] : '';
                 $list[] = $r;
                 break;
         }
@@ -133,18 +157,14 @@ class RapToPostman
      */
     private function requestType($type)
     {
-        $request = '';
-        switch ($type) {
-            case '1':
-                $request = 'GET';break;
-            case '2':
-                $request = 'POST';break;
-            case '3':
-                $request = 'PUT';break;
-            case '4':
-                $request = 'DELETE';break;
-        }
-        return $request;
+        $requestType = [
+            1 => 'GET',
+            2 => 'POST',
+            3 => 'PUT',
+            4 => 'DELETE',
+        ];
+
+        return $requestType[$type];
     }
 
     /**
@@ -153,17 +173,18 @@ class RapToPostman
     private function requestHeader()
     {
         $data = [
-            'Authorization'   => ['Bearer {{access_token}}', '访问令牌'],
-            'X-Requested-With' => ['XMLHttpRequest', '异步访问']
+            'Authorization' => ['Bearer {{access_token}}', '访问令牌'],
+            'X-Requested-With' => ['XMLHttpRequest', '异步访问'],
         ];
         $header = [];
         foreach ($data as $k => $v) {
             $header[] = [
-                "key"         => $k,
-                "value"       => $v[0],
-                "description" => $v[1]
+                "key" => $k,
+                "value" => $v[0],
+                "description" => $v[1],
             ];
         }
+
         return $header;
     }
 
@@ -173,19 +194,19 @@ class RapToPostman
     private function event()
     {
         $event[] = [
-                "listen" => "test",
-                "script" => [
-                    "type" => "text/javascript",
-                    "exec" => [
-                        "if (responseCode.code == 200) {",
-                        "    var jsonData = JSON.parse(responseBody);",
-                        "    tests['success'] = responseCode.code == 201;",
-                        "    postman.setEnvironmentVariable('access_token', jsonData.data.access_token); // environment variable";
-                        "}"
-                    ]
-                ]
+            "listen" => "test",
+            "script" => [
+                "type" => "text/javascript",
+                "exec" => [
+                    "if (responseCode.code >= 200 && responseCode.code < 300) {",
+                    "    var jsonData = JSON.parse(responseBody);",
+                    "    tests['success'] = responseCode.code >= 200 && responseCode.code < 300;",
+                    "    postman.setEnvironmentVariable('access_token', jsonData.data.access_token); // environment variable",
+                    "}",
+                ],
+            ],
         ];
+
         return $event;
     }
 }
-?>
